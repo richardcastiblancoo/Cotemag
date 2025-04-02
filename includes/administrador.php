2c24aed338cb0d2253e56861363bb7b1105a6e07
Anibal <stacruz25ams@gmail.com>
2025-04-02 09:40:51 -0500

    boton para prueba

--- includes/administrador.php
+++ includes/administrador.php
@@ -1,6 +1,18 @@
 <a href="">Administrador</a>
+<a href="/pages/oferta_academica.php" class="btn2">Programas ofertados</a>
 
 <style>
+        .btn2{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-decoration: none;
+        background-color:rgb(168, 43, 31);
+        color: wheat;
+        width: 150px;
+        height: 30px;
+        margin-top: 20px;
+    }
     a{
         display: flex;
         justify-content: center;

0b762803e018b5ca5161e1869276b078176a3864
richardcastiblancoo <castiblancorichard8@email.com>
2025-04-13 11:56:23 -0500

    agregando contenido de redes al footer

--- includes/footer.php
+++ includes/footer.php
@@ -24,8 +24,8 @@
                 <ul>
                     <li><a href="#">PQR</a></li>
                     <li><a href="#">Blog Noticias</a></li>
-                    <li><a href="#">(+57) 3004813444</a></li>
-                    <li><a href="#">direcciongeneral@cotemag.edu.co</a></li>
+                    <li><a href="https://wa.me/573004813444" target="_blank">(+57) 3004813444</a></li>
+                    <li><a href="mailto:direcciongeneral@cotemag.edu.co">direcciongeneral@cotemag.edu.co</a></li>
                     <li><a href="#">Avenida Los Estudiantes #17 - 46</a></li>
                 </ul>
             </div>
@@ -33,11 +33,10 @@
             <div class="footer-column">
                 <h3>Redes Sociales</h3>
                 <ul>
-                    <li><a href="#"><i class="fab fa-facebook"></i> Facebook</a></li>
-                    <li><a href="#"><i class="fab fa-instagram"></i> Instagram</a></li>
-                    <li><a href="#"><i class="fab fa-twitter"></i> Twitter</a></li>
-                    <li><a href="#"><i class="fab fa-youtube"></i> YouTube</a></li>
-                    <li><a href="#"><i class="fab fa-whatsapp"></i> WhatsApp</a></li>
+                    <li><a href="https://www.facebook.com/cotemagsm"><i class="fab fa-facebook"></i> Facebook</a></li>
+                    <li><a href="https://www.instagram.com/cotemagsm/"><i class="fab fa-instagram"></i> Instagram</a></li>
+                    <li><a href="https://x.com/cotemagsm"><i class="fab fa-twitter"></i> Twitter</a></li>
+                    <li><a href="https://wa.me/573004813444" target="_blank"><i class="fab fa-whatsapp"></i> WhatsApp</a></li>
                 </ul>
             </div>
         </div>

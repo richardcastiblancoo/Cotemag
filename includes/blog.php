4978d6ef8b14de29226815987cc7739fb830da2f
richardcastiblancoo <castiblancorichard8@email.com>
2025-04-13 11:47:51 -0500

    agregando los botones de ver mas posts

--- includes/blog.php
+++ includes/blog.php
@@ -15,8 +15,12 @@ $resultado = mysqli_query($conexion, $query);
 <div class="container mt-4">
     <h1 class="text-center mb-4">Blog Cotemag</h1>
     <div class="row">
-        <?php while ($post = mysqli_fetch_assoc($resultado)): ?>
-            <div class="col-md-4">
+        <?php 
+        $counter = 0;
+        while ($post = mysqli_fetch_assoc($resultado)): 
+            $visibility = $counter >= 3 ? 'style="display: none;"' : '';
+        ?>
+            <div class="col-md-4 post-item" <?php echo $visibility; ?>>
                 <div class="card post-card mb-4">
                     <?php if (!empty($post['imagen'])): ?>
                         <img src="<?php echo $post['imagen']; ?>" class="card-img-top post-image" alt="Post image">
@@ -34,6 +38,42 @@ $resultado = mysqli_query($conexion, $query);
                     </div>
                 </div>
             </div>
-        <?php endwhile; ?>
+        <?php 
+            $counter++;
+        endwhile; 
+        ?>
     </div>
+    
+    <?php if ($counter > 3): ?>
+        <div class="text-center mt-4 mb-5">
+            <button id="loadMore" class="btn btn-outline-primary btn-lg">
+                <i class="fas fa-plus-circle mr-2"></i>Ver más posts
+            </button>
+        </div>
+    <?php endif; ?>
 </div>
+
+<!-- Add jQuery if not already included -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<script>
+    jQuery(document).ready(function($) {
+        let postsShown = 3;
+        const postsPerLoad = 3;
+        const posts = $('.post-item');
+        const totalPosts = posts.length;
+        
+        if (totalPosts <= 3) {
+            $('#loadMore').hide();
+        }
+        
+        $('#loadMore').on('click', function() {
+            posts.slice(postsShown, postsShown + postsPerLoad).fadeIn('slow');
+            postsShown += postsPerLoad;
+            
+            if (postsShown >= totalPosts) {
+                $(this).fadeOut('slow');
+            }
+        });
+    });
+</script>

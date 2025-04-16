687ba34b6dd225b04a774d3b9d819e14dc9aedda
richardcastiblancoo <castiblancorichard8@email.com>
2025-04-16 11:24:38 -0500

    cambio de img del about

--- includes/about.php
+++ includes/about.php
@@ -1,9 +1,9 @@
 <main class="main">
-<div class="hero-section">
+    <div class="hero-section">
         <h1 class="title">La <span class="gradient-text">opción</span><br>inteligente para <span class="gradient-text">el futuro</span></h1>
-        
+
         <p class="description">Somos una Institución con amplia trayectoria educativa y reconocidos a nivel nacional por nuestra calidad en el servicio.</p>
-        
+
         <div class="search-container">
             <input type="text" class="search-input" placeholder="Buscar carreras tecnicas">
             <button class="search-button">Continue</button>

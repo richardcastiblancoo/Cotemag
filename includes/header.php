9575c96c7b921f8f2559c506430653ad2b3ebb3a
richardcastiblancoo <castiblancorichard8@email.com>
2025-04-22 14:54:06 -0500

    rediseñando el diseño del header

--- includes/header.php
+++ includes/header.php
@@ -1,25 +1,24 @@
-<header class="header">
-    <div class="text-img">
+<header class="encabezado">
+    <div class="contenedor-logo">
         <img src="/Cotemag/assets/img/cotemag.png" class="logo" alt="logo-cotemag">
-        
     </div>
-    <nav class="navbar">
+    <nav class="navegacion">
         <ul>
-            <li><a class="contant-a" title="Inicio" href="#">Inicio</a></li>
-            <li><a class="contant-a" title="Novedades" href="#">Novedades</a></li>
-            <li><a class="contant-a" title="Conocenos" href="#">Conocenos</a></li>
-            <li><a class="contant-a" title="Programas" href="#">Programas</a></li>
-            <li><a class="contant-a" title="Contacto" href="#">Contacto</a></li>
+            <li><a class="enlace-nav" title="Inicio" href="#">Inicio</a></li>
+            <li><a class="enlace-nav" title="Novedades" href="#">Novedades</a></li>
+            <li><a class="enlace-nav" title="Conocenos" href="#">Conocenos</a></li>
+            <li><a class="enlace-nav" title="Programas" href="#">Programas</a></li>
+            <li><a class="enlace-nav" title="Contacto" href="#">Contacto</a></li>
         </ul>
     </nav>
-    <div class="container-botones">
-        <div class="auth-buttons">
+    <div class="contenedor-botones">
+        <div class="botones-autenticacion">
             <?php if (isset($_SESSION['user_id'])): ?>
-                <a href="dashboard.php" class="btn btn-outline-light mr-2">Dashboard</a>
-                <a href="logout.php" class="btn btn-outline-light">Cerrar Sesión</a>
+                <a href="dashboard.php" class="boton-iniciar">Dashboard</a>
+                <a href="logout.php" class="boton-iniciar">Cerrar Sesión</a>
             <?php else: ?>
-                <a href="login.php" class="sign-in-btn">Admin Blog</a>
-                <a href="https://site4.q10.com/login?ReturnUrl=%2F&aplentId=73c46535-d1df-4c30-8340-44c2a135aae5" class="sign-in-btn">Login | Q10</a>
+                <a href="login.php" class="boton-iniciar">Admin Blog</a>
+                <a href="https://site4.q10.com/login?ReturnUrl=%2F&aplentId=73c46535-d1df-4c30-8340-44c2a135aae5" class="boton-iniciar">Login | Q10</a>
             <?php endif; ?>
         </div>
     </div>
--- scripts/scroll.js
+++
@@ -1,9 +0,0 @@
-//scroll effect
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('.header');
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
